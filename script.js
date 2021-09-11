db5cf1d152bafd48390fd4c0408e6a20ccc48ba4
Jagdish Saini <jsaini8941@gmail.com>
2021-09-11 09:49:16 +0530

    commit

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const output = document.querySelector('#output');
 const button = document.querySelector('#button');
 
 button.addEventListener('click', async () => {
+  output.innerHTML = "Translating in progress...."
   await fetch(`https://api.funtranslations.com/translate/minion.json?text=${input.value}`)
     .then(res => res.json())
-    .then(res => output.innerText = res.contents.translated)
+    .then(res => console.log(res))
+  .catch(error => output.innerText = error)
 })
